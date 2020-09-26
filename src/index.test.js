330bae268c67d0bfbc03fb8c0427384876d1da5b
François Rouault <francois.rouault@cocoricostudio.com>
2020-09-26 15:56:18 +0200

    A test to test

---
+++ src/index.test.js
@@ -0,0 +1,5 @@
+var { sum } = require("../dist/index");
+
+test("adds 1 + 2 to equal 3", () => {
+  expect(sum(1, 2)).toBe(3);
+});
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+function sum(a: number, b: number) {
+  return a + b;
+}
+
+module.exports = {
+  sum,
+};
