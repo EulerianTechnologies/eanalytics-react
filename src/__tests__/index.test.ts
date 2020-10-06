4bebd0f2202acef4655f48f2924e3235258908c4
François Rouault <francois.rouault@cocoricostudio.com>
2020-10-06 16:35:46 +0200

    Insert initialized eulerian tag in document's head

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,10 +1,45 @@
+import { JSDOM } from "jsdom";
 import EAnalytics from "../eanalytics";
 
-test("eulerian is truthy", () => {
-  expect(EAnalytics).toBeTruthy();
+beforeEach(() => {
+  const dom = new JSDOM();
+
+  // @ts-ignore
+  global.document = dom.window.document;
+  // @ts-ignore
+  global.window = dom.window;
 });
 
-test("eulerian can initialize with host", () => {
-  EAnalytics.initialize("ett.host.com");
-  expect(EAnalytics.host).toBe("ett.host.com");
+describe("Initialize function", () => {
+  test("inserts EA tag in <head>", () => {
+    // Given
+    const host = "et1.eulerian.com";
+
+    // When
+    EAnalytics.initialize(host);
+
+    // Then
+    expect(document.head.children.length).toBe(1);
+    expect(document.head.firstElementChild.innerHTML).toBe(
+      "(function(e,a){var i=e.length,y=5381,k='script',s=window,v=document,o=v.createElement(k);for(;i;){i-=1;y=(y*33)^e.charCodeAt(i)}y='_EA_'+(y>>>=0);(function(e,a,s,y){s[a]=s[a]||function(){(s[y]=s[y]||[]).push(arguments);s[y].eah=e;};}(e,a,s,y));i=new Date/1E7|0;o.ea=y;y=i%26;o.async=1;o.src='//'+e+'/'+String.fromCharCode(97+y,122-y,65+y)+(i%1E3)+'.js?2';s=v.getElementsByTagName(k)[0];s.parentNode.insertBefore(o,s);})('et1.eulerian.com', 'EA_collector');"
+    );
+  });
+
+  test("inserts EA tag at position 0", () => {
+    // Given
+    const createDummyScriptElement = () => {
+      let dummy = document.createElement("script");
+      dummy.innerHTML = "dummy";
+      return dummy;
+    };
+    document.head.appendChild(createDummyScriptElement());
+    document.head.appendChild(createDummyScriptElement());
+
+    // When
+    EAnalytics.initialize("i.am.host");
+
+    // Then
+    expect(document.head.children.length).toBe(3);
+    expect(document.head.firstElementChild.innerHTML).not.toBe("dummy");
+  });
 });
--- src/eanalytics.ts
+++ src/eanalytics.ts
@@ -1,22 +1,24 @@
-var eanalytics = (function () {
-  const ea = {
-    host: undefined as string,
+const eaTag = (host: string) =>
+  `(function(e,a){var i=e.length,y=5381,k='script',s=window,v=document,o=v.createElement(k);for(;i;){i-=1;y=(y*33)^e.charCodeAt(i)}y='_EA_'+(y>>>=0);(function(e,a,s,y){s[a]=s[a]||function(){(s[y]=s[y]||[]).push(arguments);s[y].eah=e;};}(e,a,s,y));i=new Date/1E7|0;o.ea=y;y=i%26;o.async=1;o.src='//'+e+'/'+String.fromCharCode(97+y,122-y,65+y)+(i%1E3)+'.js?2';s=v.getElementsByTagName(k)[0];s.parentNode.insertBefore(o,s);})('${host}', 'EA_collector');`;
 
-    /**
-     * Initializes the Eulerian Analytics SDK.
-     * @param host The host provided by Eulerian.
-     */
-    initialize: (host: string) => {
-      ea.host = host;
-    },
+const ea = (host: string) => {
+  const script = () => {
+    const script = document.createElement("script");
+    script.innerHTML = eaTag(host);
+    return script;
   };
+  return { script };
+};
 
-  return ea;
-})();
+const EAnalytics = {
+  /**
+   * Insert Eulerian Analytics tag in the <head> of the current document.
+   * @param host The host provided by Eulerian.
+   */
+  initialize: (host: string) => {
+    const _ea = ea(host);
+    document.head.insertBefore(_ea.script(), document.head.firstElementChild);
+  },
+};
 
-/**
- * The base payload for any Eulerian property.
- */
-type EAPropertiess = { [key: string]: string };
-
-export default eanalytics;
+export default EAnalytics;
